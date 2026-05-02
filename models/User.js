65595a8b355d2aa39a0d017f808e1f5b2b95a640
Yash Sarankar <yashshaft@gmail.com>
2026-05-02 13:05:04 +0530

    feat: create User Mongoose model with phone authentication and KYC fields

--- models/User.js
+++ models/User.js
@@ -42,6 +42,10 @@ const userSchema = new mongoose.Schema({
       type: String,
       trim: true
     },
+    state: {
+      type: String,
+      trim: true
+    },
     pincode: {
       type: String,
       trim: true
